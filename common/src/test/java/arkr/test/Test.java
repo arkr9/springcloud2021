0f64f48a3fed4a84edaca572665fda26bf4ae94c
arkr <arkr@vvy.com>
2021-12-06 21:50:37 +0800

    添加到master分支

--- common/src/test/java/arkr/test/Test.java
+++ common/src/test/java/arkr/test/Test.java
@@ -7,7 +7,7 @@ package arkr.test;
  **/
 public class Test {
     public static void main(String[] args) {
-        System.out.println("hello git");
+        System.out.println("hello git branch");
     }
 
 }
